634f122b50e04f8ec3b56ab15fa49a01db238d6d
shahinbary-pixel <shahinbary@gmail.com>
2026-08-08 02:35:43 +0600

    Add service worker for asset caching
    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'textile-erp-v1';
+const assetsToCache = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
