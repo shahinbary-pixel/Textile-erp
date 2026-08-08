8925c22ba80fb79fdc7d13db4c57647f74406537
shahinbary-pixel <shahinbary@gmail.com>
2026-08-08 13:36:33 +0600

    Refactor service worker for caching and fetching

--- sw.js
+++ sw.js
@@ -1,21 +1,90 @@
-const CACHE_NAME = 'textile-erp-v1';
-const assetsToCache = [
-  '/',
-  '/index.html'
+const CACHE_NAME = "textile-erp-v1";
+
+const ASSETS_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assetsToCache);
-    })
-  );
+
+// Install
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+            .then(() => {
+                return self.skipWaiting();
+            })
+    );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+
+// Activate
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys()
+            .then((cacheNames) => {
+                return Promise.all(
+                    cacheNames
+                        .filter((cacheName) => cacheName !== CACHE_NAME)
+                        .map((cacheName) => caches.delete(cacheName))
+                );
+            })
+            .then(() => {
+                return self.clients.claim();
+            })
+    );
+});
+
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request)
+            .then((cachedResponse) => {
+
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(event.request)
+                    .then((networkResponse) => {
+
+                        if (
+                            !networkResponse ||
+                            networkResponse.status !== 200 ||
+                            networkResponse.type === "opaque"
+                        ) {
+                            return networkResponse;
+                        }
+
+                        const responseToCache =
+                            networkResponse.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(
+                                    event.request,
+                                    responseToCache
+                                );
+                            });
+
+                        return networkResponse;
+                    })
+                    .catch(() => {
+
+                        return caches.match("./index.html");
+
+                    });
+            })
+    );
 });
